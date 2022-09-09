e6d6898864651f815b439dd7f49c688250659e87
HelenaHalldinITHS <helena.halldin@iths.se>
2022-09-09 09:54:40 +0200

    Make start point for project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
+  <h1>Workout plan</h1>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
+h1 {
+  margin-bottom: 0px;
+  background-color: grey;
+  color: white;
+}
 nav {
-  padding: 30px;
+  padding: 5px;
+  background-color: lightgray;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 20px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: gray;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: black;
 }
 </style>
